5c322ed7efde67a42b664f8fc6244f1e199ac7ef
vadim <vadim.zubairov1993@gmail.com>
2020-11-17 19:57:05 +0500

    'oneday'

---
+++ src/pages/Contacts/ContactsTable/index.js
@@ -0,0 +1,3 @@
+export const ContactsTable = ({ data }) => {
+  return <div>{data[0].name.first}</div>;
+};
--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,46 @@
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { useContacts } from "./useContacts";
+
+import { ContactsTable } from "./ContactsTable";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      marginTop: theme.spacing(4),
+    },
+    headContainer: {
+      marginBottom: theme.spacing(3),
+    },
+  })
+);
+
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  const contacts = useContacts();
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} className={classes.headContainer}>
+          <Typography variant="h3" component="h1">
+            Contacts
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          {(() => {
+            if (contacts.isLoading) {
+              return <div>...Loading</div>;
+            }
+            if (contacts.isError) {
+              return <div>...Error</div>;
+            }
+            return <ContactsTable data={contacts.data} />;
+          })()}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
